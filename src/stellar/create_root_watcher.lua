a71381c1654bca58bdeea4adda35cd800a8041a3
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-25 18:21:25 -0300

    start working

--- src/stellar/create_root_watcher.lua
+++ src/stellar/create_root_watcher.lua
@@ -1,5 +1,6 @@
 stellar.create_root_watcher = function(output_file)
     local selfobj = {}
+    selfobj.trace = {}
     local file = stellar.required_functions.open(output_file, "w")
 
     if not file then
@@ -16,6 +17,28 @@ stellar.create_root_watcher = function(output_file)
             return
         end
         file:write(data)
+    end
+    selfobj.get_trace_path = function()
+        if #selfobj.trace == 0 then
+            return "global"
+        end
+        local trace = ""
+        for i = 1, #selfobj.trace do
+            trace = trace .. "[" .. selfobj.trace[i] .. "]"
+        end
+        return trace
+    end
+    selfobj.create_function = function(name, callback)
+        return function(args)
+            selfobj.trace[#selfobj.trace + 1] = name
+            selfobj.stream("trace change:")
+            selfobj.stream(selfobj.get_trace_path() .. "\n")
+            local result = callback(args)
+            selfobj.trace[#selfobj.trace] = nil
+            selfobj.stream("trace change:")
+            selfobj.stream(selfobj.get_trace_path() .. "\n")
+            return result
+        end
     end
     return selfobj
 end
--- teste.lua
+++ teste.lua
@@ -1,7 +1,17 @@
 stellar = require("stellar")
 
 watcher = stellar.create_root_watcher("debug.txt")
-watcher.stream("teste")
-watcher.stream("teste")
-watcher.stream("teste")
-watcher.stream("teste")
+
+b = watcher.create_function("b", function(x)
+    for i = 1, 10 do
+        watcher.stream("plot:b[7]\n")
+        watcher.stream("i = " .. i .. "\n")
+    end
+end)
+
+a = watcher.create_function("a", function(x)
+    b()
+end)
+
+
+a(1)
